a40519d8eba5f4d24c4d295eeb1ba12fead6a31d
Arón Miguel Hernández Martínez <99159072+WearierLamb1@users.noreply.github.com>
2023-03-15 14:55:37 -0600

    Add files via upload

---
+++ Comandos para instalar los recursos de la paqueteria prob.R
@@ -0,0 +1,17 @@
+#Ibstalar primero RTools
+
+#Comando necesarios para instalar los paquetes y dependencias
+#Dependencias
+install.packages("timeDate", dependencies = TRUE)
+install.packages("timeSeries", dependencies = TRUE)
+install.packages("timefBasics", dependencies = TRUE)
+install.packages("timefOptions", dependencies = TRUE)
+
+#Paquetes 
+install.packages("C:/ruta al documento", repos = NULL, type = "source")
+
+#Paqueterias 
+fAsianOptions
+prob
+
+#La instalación debe de hacerse desde la consola de RStudio
